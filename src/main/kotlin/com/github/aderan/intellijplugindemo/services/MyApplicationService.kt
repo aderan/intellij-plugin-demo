0e750167cdea79643b7594b72b61a82abd853fc9
GitHub Action <action@github.com>
2021-01-08 08:58:41 +0000

    Template cleanup

--- src/main/kotlin/com/github/aderan/intellijplugindemo/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/aderan/intellijplugindemo/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.aderan.intellijplugindemo.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.aderan.intellijplugindemo.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/aderan/intellijplugindemo/services/MyProjectService.kt
+++ src/main/kotlin/com/github/aderan/intellijplugindemo/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.aderan.intellijplugindemo.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.aderan.intellijplugindemo.MyBundle
 
 class MyProjectService(project: Project) {
 
